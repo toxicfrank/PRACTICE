70623e64f08d2a93b75c2573698b5a628648faf9
Anonymous <anonymous@fsociety>
2023-03-26 21:03:49 +0100

    practice

--- practice.c
+++ practice.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 /**
- * main - main function
+ * main - this is a programme that will display the salary of an
+ * employer when inserted
  *
  * Return: always 0
  */
-
 int main(void)
 {
-	int age = 25;
+	double salary;
 
-	printf("%p\n", &age);
+	printf("Enter your salary: \n");
 
-	int *ptr;
+	scanf("%lf", &salary);
 
-	printf("%p\n", ptr);
+	double *ptr = &salary;
 
-	printf("Hello world\n");
+	printf("Your salary is: %.2lf\n", *ptr);
+
+	double new_salary = *ptr * 2;
+
+	printf("Your double salary is: %.2lf\n", new_salary);
 
 	return (0);
+
+
 }
