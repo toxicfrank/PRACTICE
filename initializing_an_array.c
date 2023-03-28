7a99fe9d84ec8b9199ee4c487586f94ebd3336c1
Anonymous <anonymous@fsociety>
2023-03-28 19:57:40 +0100

    practice

---
+++ initializing_an_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is practice
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int arr[5], i;
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	return (0);
+}
