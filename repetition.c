2f6118d30f19cc81f42aee2868ba1b6d5be66223
Anonymous <anonymous@fsociety>
2023-03-31 07:17:36 +0100

    practice

--- repetition.c
+++ repetition.c
@@ -8,7 +8,31 @@
 
 int main(void)
 {
-	
-	
+	/*Part 1*/
+	int seen[10] = {0};
+	int N;
+
+	printf("Enter the number: ");
+	scanf("%d", &N);
+
+	/*Part 2*/
+	int rem;
+
+	while (N > 0)
+	{
+		rem = N % 10;
+		if (seen[rem] == 1)
+		break;
+		seen[rem] = 1;
+		N = N / 10;
+	}
+	if (N > 0)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
 	return (0);
 }
